24aa0970d69925c65d5ba0e71ec596142c09b7ed
mhendrick98 <rsfmike@hotmail.com>
2016-11-16 22:07:09 -0500

    2LIT
    2LIT Final

---
+++ 2lit🔥💯/Dot Class.swift
@@ -0,0 +1,17 @@
+//
+//  Dot Class.swift
+//  2lit🔥💯
+//
+//  Created by Michael Hendrick on 11/12/16.
+//  Copyright © 2016 YHack. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+class Dot: SKSpriteNode{
+    init(color: UIColor, size: CGSize) {
+        self.color = red
+        self.size = (width: 30, height: 30)
+}
---
+++ practice.swift
@@ -0,0 +1,7 @@
+//
+//  practice.swift
+//  2lit🔥💯
+//
+//  Created by Michael Hendrick on 11/11/16.
+//  Copyright © 2016 YHack. All rights reserved.
+//
